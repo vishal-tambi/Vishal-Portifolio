dda0ce11cb94e1f21af850b26b17b432854c82d4
Vishal Tambi <tambivishal3@gmail.com>
2024-04-30 08:01:07 +0530

    fix it

--- connect.php
+++ connect.php
@@ -1,24 +1,24 @@
-<?php
-	$name = $_POST['name'];
-	$subject = $_POST['subject'];
-	// $lastName = $_POST['lastName'];
-	// $gender = $_POST['gender'];
-	$email = $_POST['email'];
-	// $password = $_POST['password'];
-	$message = $_POST['message'];
+        <?php
+            $name = $_POST['name'];
+            $subject = $_POST['subject'];
+            // $lastName = $_POST['lastName'];
+            // $gender = $_POST['gender'];
+            $email = $_POST['email'];
+            // $password = $_POST['password'];
+            $message = $_POST['message'];
 
-	// Database connection
-	$conn = new mysqli('localhost','root','','connect');
-	if($conn->connect_error){
-		echo "$conn->connect_error";
-		die("Connection Failed : ". $conn->connect_error);
-	} else {
-		$stmt = $conn->prepare("insert into connect_table(name, email, subject, message) values(?, ?, ?, ?)");
-		$stmt->bind_param("ssss", $name, $email, $subject, $message);
-		$execval = $stmt->execute();
-		echo $execval;
-		echo "Your message reached successfully...";
-		$stmt->close();
-		$conn->close();
-	}
-?>
+            // Database connection
+            $conn = new mysqli('localhost','root','','connect');
+            if($conn->connect_error){
+                echo "$conn->connect_error";
+                die("Connection Failed : ". $conn->connect_error);
+            } else {
+                $stmt = $conn->prepare("insert into connect_table(name, email, subject, message) values(?, ?, ?, ?)");
+                $stmt->bind_param("ssss", $name, $email, $subject, $message);
+                $execval = $stmt->execute();
+                echo $execval;
+                echo "Your message reached successfully...";
+                $stmt->close();
+                $conn->close();
+            }
+                ?>
